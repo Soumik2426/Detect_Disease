4d9fd8a72737e82b8c6c79409fb8102518abb13a
Soumik2426 <soumikmaity2426@gmail.com>
2025-11-03 20:59:38 +0530

    Removed the uvicon part from app.py

--- API/App/app.py
+++ API/App/app.py
@@ -138,9 +138,3 @@ async def predict(file: UploadFile = File(...)):
             gc.collect()
 
     return {"Class": predicted_class, "Confidence": confidence}
-
-# ------------------ Main ------------------
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
